340c76a0e61fc4a2443688689890a21ec7457a9d
Nikolai <nikolaichub.me@gmail.com>
2022-02-15 15:12:28 +0200

    more comprehensible check of discriminant

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const calculate = (a, b, c) => {
   if (discriminant < 0) return console.log('There are 0 roots');
   const x1 = (-b - Math.sqrt(discriminant)) / (a * 2);
   const x2 = (-b + Math.sqrt(discriminant)) / (a * 2);
-  const message = !discriminant
-    ? `There is 1 root\nx1 = ${x1}`
-    : `There are 2 roots\nx1 = ${x1}\nx2 = ${x2}`;
+  const message =
+    discriminant === 0
+      ? `There is 1 root\nx1 = ${x1}`
+      : `There are 2 roots\nx1 = ${x1}\nx2 = ${x2}`;
   return console.log(message);
 };
 
